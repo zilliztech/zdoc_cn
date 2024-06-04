9e78cc1f6ed52ea120ce814c8f131bfefb81aa59
Shuyou <shuyou.zhan@zilliz.com>
2024-06-04 10:41:34 +0800

    fix: husky

--- scripts/pre-check.js
+++ scripts/pre-check.js
@@ -16,7 +16,7 @@ fs.readFile(nginxConfigPath, "utf8", (err, fileContents) => {
 
 	const redirects = getNginxRedirects(fileContents);
 
-	const GIT_COMMAND_GET_CHANGED_FILES = `git diff --cached --name-only`;
+	const GIT_COMMAND_GET_CHANGED_FILES = `git diff --cached --name-only --diff-filter=DR`;
 	exec(GIT_COMMAND_GET_CHANGED_FILES, (error, stdout, stderr) => {
 		if (error) {
 			console.error(`exec error: ${error}`);
