a9f18ba0baee405c60f03e6b000e13a9c0c0cdbd
AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>
2025-05-19 23:17:11 +0800

    update docs
    Signed-off-by: AnthonyTsu1984 <115786031+AnthonyTsu1984@users.noreply.github.com>

--- src/components/Supademo/index.js
+++ src/components/Supademo/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import styles from './styles.module.css';
 
-export default function Supademo({ id, title }) {
-    const src = `https://app.supademo.com/embed/${id}?embed_v=2`
+export default function Supademo({ id, title, isShowcase }) {
+    let src = `https://app.supademo.com/embed/${id}?embed_v=2`
 
+    if (isShowcase) {
+        src = `https://app.supademo.com/showcase/embed/${id}?embed_v=2`
+
+        return (
+            <div className={styles.container}>
+                <iframe src={src}
+                    data-version="2"
+                    loading="lazy" 
+                    title={title} 
+                    allow="clipboard-write" 
+                    frameborder="0" 
+                    webkitallowfullscreen="true" 
+                    mozallowfullscreen="true" 
+                    allowfullscreen 
+                    className={styles.iframe} />
+            </div>
+        )
+    }
 
     return (
         <div className={styles.container}>
-            <iframe src={src} 
+            <iframe src={src}
                 loading="lazy" 
                 title={title} 
                 allow="clipboard-write" 
