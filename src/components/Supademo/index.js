6b4e57e1bafae3935272d2df5e5711a0abe45021
AnthonyTsu1984 <wenjia.tu@zilliz.com>
2026-02-10 14:07:03 +0800

    update styles
    Signed-off-by: AnthonyTsu1984 <wenjia.tu@zilliz.com>

--- src/components/Supademo/index.js
+++ src/components/Supademo/index.js
@@ -8,32 +8,33 @@ export default function Supademo({ id, title, isShowcase }) {
         src = `https://app.supademo.com/showcase/embed/${id}?embed_v=2`
 
         return (
-            <div className={styles.container}>
+            <div className={`${styles.container} supademo-container`}>
                 <iframe src={src}
                     data-version="2"
-                    loading="lazy" 
-                    title={title} 
-                    allow="clipboard-write" 
-                    frameborder="0" 
-                    webkitallowfullscreen="true" 
-                    mozallowfullscreen="true" 
-                    allowfullscreen 
+                    loading="lazy"
+                    title={title}
+                    allow="clipboard-write; fullscreen"
+                    frameborder="0"
+                    webkitallowfullscreen="true"
+                    mozallowfullscreen="true"
+                    allowfullscreen
                     className={styles.iframe} />
             </div>
         )
     }
 
     return (
-        <div className={styles.container}>
+        <div className={`${styles.container} supademo-container`}>
             <iframe src={src}
-                loading="lazy" 
-                title={title} 
-                allow="clipboard-write" 
-                frameborder="0" 
-                webkitallowfullscreen="true" 
-                mozallowfullscreen="true" 
-                allowfullscreen 
+                loading="lazy"
+                title={title}
+                allow="clipboard-write; fullscreen"
+                frameborder="0"
+                webkitallowfullscreen="true"
+                mozallowfullscreen="true"
+                allowfullscreen
                 className={styles.iframe} />
         </div>
     )
 }
+
